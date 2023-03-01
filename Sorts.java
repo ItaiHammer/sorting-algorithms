9d89b951e71a240a5d5e749d1c38edf1d2786d58
Itai Hammer <61294759+ItaiHammer@users.noreply.github.com>
2023-02-28 22:15:46 -0800

    laptop changes

--- Sorts.java
+++ Sorts.java
@@ -24,28 +24,10 @@ public class Sorts {
             int[] test = tests.get(i);
             System.out.println("===== Test #" + i + "=========");
             System.out.println("Before: " + Arrays.toString(test));
-            test = selectionSort(test);
             System.out.println("After: " + Arrays.toString(test));
             System.out.println("================================");
         }
     }
 
-    private static int[] selectionSort(int[] arr) {
-        for (int i = 0; i < arr.length-1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[minIndex]) {
-                    minIndex = j;
-                }
-            }
 
-            int temp = arr[minIndex];
-            arr[minIndex] = arr[i];
-            arr[i] = temp;
-        }
-
-        return arr;
-    }
-
-    
 }
